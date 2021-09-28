3205e4d2f2abe446392bb4ce321ea6e5581667b0
Kevin Fernando Yataco Vera <keve18@live.com>
2021-09-27 21:34:12 -0500

    Primer avance de vista Drawback-desktop

---
+++ Drawback/pages/js/empezarSimulacion.js
@@ -0,0 +1,9 @@
+var viewEmpezar = document.getElementById("view-empezar-principal")
+var viewSimulador = document.getElementById("view-simulador-principal")
+
+
+
+function empezarSimulacion() {
+    viewEmpezar.className = "d-none"
+    viewSimulador.className = "d-display"
+}
