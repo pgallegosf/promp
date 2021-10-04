30c77b2511632595412417113b99e7cb244e034b
Kevin Fernando Yataco Vera <keve18@live.com>
2021-10-04 08:56:53 -0500

    Avance de vista desktop-costos

---
+++ Costo/pages/js/EmpezarSimulacionCosto.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    $("#div-boton").click(function() {
+        mostrarEmpezarSecond();
+    });
+    $("#next").click(function() {
+        mostrarEmpezarTercero();
+    });
+    $("#btn-volver").click(function() {
+        volver();
+    });
+    $("#btn-volver2").click(function() {
+        volver2();
+    });
+    $("#view-empezar-second").hide();
+    $("#view-empezar-tercero").hide();
+});
+
+function mostrarEmpezarSecond() {
+    $("#view-empezar-principal").hide();
+    $("#view-empezar-second").show();
+}
+
+function mostrarEmpezarTercero() {
+    $("#view-empezar-second").hide();
+    $("#view-empezar-tercero").show();
+}
+
+function volver() {
+    $("#view-empezar-second").hide();
+    $("#view-empezar-principal").show();
+}
+
+function volver2() {
+    $("#view-empezar-tercero").hide();
+    $("#view-empezar-second").show();
+}
---
+++ Costo/pages/js/GenerarPdfResultado.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#pdf").click(function() {
+        generarPDF();
+    });
+});
+
+function generarPDF() {
+    $("pdfResultadoSimulador").show();
+    var element = document.getElementById('pdfResultadoSimulador');
+    html2pdf(element, {
+        margin: 10,
+        filename: 'documento.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2, logging: true, dpi: 192, letterRendering: true },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    })
+}
